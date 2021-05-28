706bce4814727f7b387f48488e6d5ae31c575da7
unknown <abida.nizam11@gmail.com>
2021-05-28 20:30:05 +0500

    create a numeric, string and boolean variable and print them

---
+++ AddVariables/app.js
@@ -0,0 +1,11 @@
+// adding two variables
+
+//This is third problem
+
+// let a,b and c are variables and values are a=4, b=12 and c=a+b;
+
+
+let a=4; // variable a
+let b=12; // variable b
+ let c=a+b;  // variable c
+ console.log(c); // this is the answer
