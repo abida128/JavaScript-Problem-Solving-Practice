9f2fca1cea12a32587ad721120b71def39d3e454
unknown <abida.nizam11@gmail.com>
2021-05-28 21:24:36 +0500

    introducing buttons with different colors names and finding output

---
+++ ExternalJsbutton/app.js
@@ -0,0 +1,19 @@
+// i call different functions for different color buttons
+
+//this function is for red color button, when we click on button it will give alert
+
+function redColor(){
+    alert ("This is Red Color Button");
+}
+
+// this function is for blue color button
+
+function blueColor(){
+    alert ("This is blue Color Button");
+}
+
+//this function is for green color button
+
+function greenColor(){
+    alert ("This is green Color Button");
+}
