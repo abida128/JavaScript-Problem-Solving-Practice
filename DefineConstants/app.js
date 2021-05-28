d21e91b70f64d2ee2ecb0f7a6a50cdfa4a294180
unknown <abida.nizam11@gmail.com>
2021-05-28 21:27:09 +0500

    create a numeric, string and boolean constants and print them

---
+++ DefineConstants/app.js
@@ -0,0 +1,30 @@
+// This is second problem :Define Constants
+
+// 1. First task is for numeric variable
+
+   const  x =1200;
+    console.log(x);
+
+
+
+
+// 2. Second task is for string variable
+
+ const y= "abida nizam";
+ console.log(y);
+
+
+
+// 3. Third task is for boolean variable
+ 
+
+let isWorkDone =true;
+let isWorkNotDone = false;
+
+if(isWorkDone){
+    alert(" Work is Done");
+
+}
+else if(isWorkNotDone){
+    alert("Work is Not Done");
+}
