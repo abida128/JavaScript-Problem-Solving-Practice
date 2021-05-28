c737f9f4a9c1a5c1eab98ce4226d9d0c5c3ca308
unknown <abida.nizam11@gmail.com>
2021-05-28 21:15:27 +0500

    This problem solved by using assinment operators

---
+++ AssignmentOperatorsQ#3/app.js
@@ -0,0 +1,67 @@
+ // Assignment  Operators
+
+
+// Examples:
+
+/*Assignment   Operators
+    i. Plus and Equal to
+    ii. Minus  and Equal to
+    iii. Multiple and Equal to
+    iv. Division and Equal to
+    v. Modulus and Equal to
+    vi. Less than less than or Equal to
+    vii. AND Equal to
+    viii. OR Equal to
+
+*/
+ 
+// we define two variables 
+ 
+ 
+ let costMango = 200;
+ let costBanana = 110;
+
+ // Plus and Equal to
+ x =(costMango) += (costBanana);
+ document.write(" costMango+=costBanana ", x);
+ document.write("<br>");
+
+
+ // Minus  and Equal to
+ y =(costMango) -= (costBanana);
+ document.write(" costMango-=costBanana ", y) ;
+ document.write("<br>");
+
+
+ // Multiple and Equal to
+ z =(costMango) *= (costBanana);
+ document.write(" costMango *= costBanana ", z);
+ document.write("<br>");
+
+ // Division and Equal to
+ x1 =(costMango) /= (costBanana);
+ document.write(" costMango /= costBanana ", x1);
+ document.write("<br>");
+
+
+ // Modulus and Equal to
+ y1 =(costMango) %= (costBanana);
+ document.write(" costMango %= costBanana ", y1);
+ document.write("<br>");
+
+ // Less than less than or Equal to
+ z1 = (costBanana) <<= (costMango);
+ document.write(" costBanana <<= costMango ", z1);
+ document.write("<br>");
+
+//  AND Equal to
+x2 =(costMango) &= (costBanana);
+document.write(" costMango &= costBanana ", x2);
+document.write("<br>");
+
+// OR  Equal to
+ y2 = (costBanana) |= (costMango) ;
+ document.write(" costBanana |= costMango ", y2);
+ document.write("<br>");
+
+ 
