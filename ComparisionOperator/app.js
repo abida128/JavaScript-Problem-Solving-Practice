57e342359d627dbe4898077d6c1147e48046656b
unknown <abida.nizam11@gmail.com>
2021-05-28 21:41:28 +0500

    Example is solve by using Comparision Operator

---
+++ ComparisionOperator/app.js
@@ -0,0 +1,86 @@
+/* Solution for Comparision Operators
+    i. Equal to ==
+    ii. Equal value and Equal type ===
+    iii. Not Equal to !=
+    iv. Greater than >
+    v. Less than <
+    vi. Greater than or Equal to >=
+    vii. Less than or Equal to <=
+
+
+
+
+*/
+
+// 2. What wil be the output of following:
+
+var a=5;
+var b=10;
+var c="5";
+var x=a;
+
+// Equal to ==
+
+t1 = a == c;
+document.write(" a == c ", t1);
+document.write("<br>");
+
+
+// Equal value and Equal type ===
+
+t2 = a === c;
+document.write(" a === c ", t2);
+document.write("<br>");
+
+
+// Equal to ==
+
+t3 = a == x; 
+document.write(" a == x ", t3);
+document.write("<br>");
+
+
+// Not Equal to !=
+
+t4 = a != b; 
+document.write(" a != b ", t4);
+document.write("<br>");
+
+
+// Greater than >
+
+t5 = a > b; 
+document.write(" a > b ", t5);
+document.write("<br>");
+
+// Less than <
+
+t6 = a < b;
+document.write(" a < b ", t6);
+document.write("<br>");
+
+
+// Greater than or Equal to >=
+
+t7 = a >= b;
+document.write(" a >= b ", t7);
+document.write("<br>"); 
+
+// Less than or Equal to <=
+
+t8 = a <= b;
+document.write(" a <= b ", t8);
+document.write("<br>"); 
+
+// Greater than or Equal to >=
+
+t9 = a >= c;
+document.write(" a >= c ", t9);
+document.write("<br>"); 
+
+
+//Less than or Equal to <=
+
+t10 = a <= c;
+document.write(" a <= c ", t10);
+document.write("<br>");
