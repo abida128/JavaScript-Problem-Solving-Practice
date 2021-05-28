8fef030aad91a442c9370a84f186e0c45f48bc13
unknown <abida.nizam11@gmail.com>
2021-05-28 21:47:23 +0500

    Takes two variables and prints the sum of both variables

---
+++ OperatorSum/app.js
@@ -0,0 +1,23 @@
+// Program For two variables 
+// print the sum of both variables
+
+
+// Example:
+
+// Function Declaration
+
+function addTwoVariables(variable1, variable2){
+
+    let sum = variable1  +  variable2 ;
+
+    //return sum = variable1  +  variable2 ;
+
+    return sum;
+
+}
+
+//addTwoVariables(200,387);
+console.log(addTwoVariables(200,387));
+console.log(addTwoVariables(2,7));
+console.log(addTwoVariables(20,17));
+console.log(addTwoVariables(567,1390));
