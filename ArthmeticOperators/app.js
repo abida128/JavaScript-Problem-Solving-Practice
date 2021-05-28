7e58112e329a652687884a0a34e0cdad07b996be
unknown <abida.nizam11@gmail.com>
2021-05-28 21:09:37 +0500

    use all Arthmetic Operators to JavaScript code for solving this problem

---
+++ ArthmeticOperators/app.js
@@ -0,0 +1,56 @@
+// Arthmetic Operators 
+
+//Example for arthmetic operators, for Addition,Subtraction, Multiplication, Division and Modulus
+
+// 1. what will be the output of following:
+
+
+var x=5;
+var y=10;
+var z=15;
+
+//Addition
+
+a= x + y;
+document.write(" x + y is ", a);
+document.write("<br>");
+
+
+
+//Subtraction
+
+b = y - x;
+document.write(" y - x is",  b);
+document.write("<br>");
+
+
+
+//Multiplication
+c = x * y;
+document.write(" x * y is ", c);
+document.write("<br>");
+
+
+//Division
+ d = y/x;
+ document.write(" y/x is ", d);
+document.write("<br>");
+
+
+
+// Modulus
+ e = x % 2;
+document.write(" x % 2 is ", e);
+document.write("<br>");
+
+// Increment 
+
+f = x++;
+document.write(" x ++ is ", f);
+document.write("<br>");
+
+// Decrement
+ g = x--;
+
+document.write(" x-- is ", g);
+document.write("<br>");
