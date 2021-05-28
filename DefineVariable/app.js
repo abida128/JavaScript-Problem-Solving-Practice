b3a1c1fc1e3b11bed7b84ccd875d1f4f99adfb41
unknown <abida.nizam11@gmail.com>
2021-05-28 21:54:39 +0500

    create variables for numeric, boolean and string

---
+++ DefineVariable/app.js
@@ -0,0 +1,38 @@
+// This is for numeric variable
+
+//This is first problem Name: Define variables
+
+// 1. First task
+
+// Faiza do you have  5000 rupies? I need 5000 rupies, it's very urgent. Faiza! Yes i have. 
+
+ let rupies =5000;
+ console.log(rupies);
+
+
+ // This is for string variable
+
+ //  2. Second task
+ 
+
+ let numb="20000";
+  console.log(numb);
+
+
+
+ // This is for Boolean variable
+
+ // 3. Third task
+
+ //  let me check Tech-Krao quizes and Tasks 
+
+
+ 
+
+ let isQuizPass=true;
+ let isQuizFail=false;
+ console.log(isQuizFail);
+
+ let isTaskDone=true;
+ let isTaskNotDone=false;
+  console.log(isTaskDone);
